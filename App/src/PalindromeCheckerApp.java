1e1ae5b304342848ccb60cc06069255df7eda415
Netraveejay <netraveejay2006@gmail.com>
2026-03-02 15:59:47 +0530

    UC2 IS COMPLETED

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,6 +1,21 @@
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to palindrome App");
+        // Program starts
+
+        // Hardcoded string
+        String text = "madam";
+
+        // Reverse the string
+        String reversed = new StringBuilder(text).reverse().toString();
+
+        // Check if palindrome
+        if (text.equals(reversed)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+
+        // Program exits
     }
 }
